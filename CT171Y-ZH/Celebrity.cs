845a2924e46e46168ef794d4ae0f3f05aa3c932e
panczeldominik01 <panczdomii@gmail.com>
2024-06-12 19:46:06 +0200

    Xml beolvasás

---
+++ CT171Y-ZH/Celebrity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CT171Y_ZH
+{
+    public class Celebrity
+    {
+        public string Name { get; set; }
+        public string Nationality { get; set; }
+        public string BirthDate { get; set; }
+        public string BirthPlace { get; set; }
+        public int Age { get; set; }
+        public int NumOfFilms { get; set; }
+        public int OscarNominations { get; set; }
+        public int OscarAwards { get; set; }
+        public string Degree { get; set; }
+        public bool IsProducer { get; set; }
+    }
+    public static class CelebrityImporter
+    {
+        public static List<Celebrity> Import()
+        { 
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Celebrity>));
+            using (FileStream fs = new FileStream("celebrities.xml", FileMode.Open))
+            {
+                return (List<Celebrity>)serializer.Deserialize(fs);
+            }
+        } 
+    }
+}
---
+++ CT171Y-ZH/TopCelebrity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CT171Y_ZH
+{
+    internal class TopCelebrity
+    {
+    }
+}
