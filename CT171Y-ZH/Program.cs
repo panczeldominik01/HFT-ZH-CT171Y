cacfa5907849bd7748c1df39fde7db2137d2b10d
panczeldominik01 <panczdomii@gmail.com>
2024-06-12 19:30:24 +0200

    Test commit

---
+++ CT171Y-ZH/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CT171Y_ZH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
